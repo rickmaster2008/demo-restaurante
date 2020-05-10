c9b560dbad4ca65a3001c47cdf657a4f4cf24003
Ricardo <ricardo@pop-os.localdomain>
2020-05-09 20:10:02 -0500

    changed name of dir

--- www/models.py
+++ www/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .user.models import Customer
+from .customer.models import Customer
 from .order.models import Order, OrderItem, OrderItemChoice
 
 class Product(models.Model):
