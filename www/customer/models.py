377e12afc501522b8755303ed96be90f102febeb
Ricardo <ricardo@pop-os.localdomain>
2020-05-09 22:35:06 -0500

    modified customer model

--- www/customer/models.py
+++ www/customer/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_lenngth=100, unique=True)
+    username = models.CharField(max_length=100, unique=True)
     address = models.CharField(max_length=200, blank=True, null=True)
     interior = models.CharField(max_length=10, blank=True, null=True)
     locality = models.CharField(max_length=50, blank=True, null=True)
